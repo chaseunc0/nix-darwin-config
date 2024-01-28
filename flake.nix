784f2b9b052d922caad0bf259e6ecfe00a8930f5
chaseunc0 <radioaddition@murena.io>
2024-01-28 14:54:13 -0500

    combined home manager into flake:   flake.nix 	new file:   home-manager/hosts/air2020.nix 	new file:   home-manager/p10k.zsh

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "RadioAddition's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, unstable, ... }:
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#RadioAdditions-MacBook-Air
@@ -18,6 +19,15 @@
         system = "x86_64-darwin";
         modules = [
           ./hosts/air2020.nix
+        ];
+      };
+    };
+    homeConfigurations = {
+      "air2020" = home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = {inherit inputs;};
+        pkgs = nixpkgs.legacyPackages."x86_64-darwin";
+        modules = [
+          ./home-manager/hosts/air2020.nix
         ];
       };
     };
